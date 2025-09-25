b246d5ce31a398874b4d64ede9b6128d5805d980
ankush-cronix <ankush.bhagat@cronixweb.com>
2025-09-25 15:41:23 +0530

    stone_grill

---
+++ java.js
@@ -0,0 +1,74 @@
+const nav=document.querySelector('.navbar__menu__list');
+const menu=document.querySelector('#menus');
+menu.addEventListener('click',()=>
+{
+    nav.classList.toggle('active');
+    menu.classList.toggle('bx-x');
+})
+
+
+
+
+// .porclean__flex__content {
+//   text-align: center;
+//   background: #f9f9f9;
+//   padding: 20px;
+//   border-radius: 10px;
+// }
+// .porclean__flex__content img {
+//   max-width: 700px;
+//   width:100%;
+//   height: auto;
+//   margin: 10px 0;
+// }
+// .owl-carousel .owl-nav button {
+//   background: #000;
+//   color: #fff;
+//   padding: 5px 10px;
+//   margin: 0 5px;
+
+// }
+    // <div class="owl-carousel porclean__flex">
+
+    //   <div class="porclean__flex__content item">
+    //     <h3>CIRCLE</h3>
+    //     <img src="img/discover1.png" alt="">
+    //     <small>Mugi</small>
+    //   </div>
+
+    //   <div class="porclean__flex__content item ego">
+    //     <h3>CROSS</h3>
+    //     <img src="img/discover2.png" alt="">
+    //     <small>Star & Cross</small>
+    //   </div>
+
+    //   <div class="porclean__flex__content item cross">
+    //     <h3>HEXAGON</h3>
+    //     <img src="img/discover3.png" alt="">
+    //     <small>Watercolor</small>
+    //   </div>
+
+    //   <div class="porclean__flex__content item">
+    //     <h3>SQUARE</h3>
+    //     <img src="img/discover4.png" alt="">
+    //     <small>Avant Garde</small>
+    //   </div>
+    // </div>
+    <script>
+  {/* $(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+      loop: true,
+      margin: 20,
+      nav: true,
+      dots: true,
+      responsive: {
+        0: { items: 1 },
+        600: { items: 2 },
+        1100: { items: 3 },
+        
+      }
+    });
+  });
+</script> */}
+{/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css"></link> */}
